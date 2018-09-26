c3820b0b83fc0922b4fc950aba2011620bc5d6d8
Numa Dhamani <numadhamani@gmail.com>
2018-09-26 16:11:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'nk_sent2vec>=1.1.0'
     ],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/nk-sent2vec@843306a1f2addc839fffb8ae511e9ddb947fbf8c#egg=nk_sent2vec-1.1.0"
+        "git+https://github.com/NewKnowledge/nk-sent2vec@eaf4c5374520fe9713b43fcdfb41b28fad24ea93#egg=nk_sent2vec-1.1.0"
     ], 
     entry_points={
         'd3m.primitives': [
