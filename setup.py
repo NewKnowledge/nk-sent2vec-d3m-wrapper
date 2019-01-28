295a93cf5a3b71eb9d37cdd7e3d1ffa3b16b3559
Numa Dhamani <numadhamani@gmail.com>
2019-01-28 13:24:49 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ setup(
     packages=['nk_sent2vec_wrapper'],
     include_package_data=True,
     install_requires=[
-        'pandas>=0.22.0',
-        'numpy>=1.14.1',
+        'pandas>=0.22.0, <=0.23.4',
+        'numpy>=1.14.1, <=1.15.4',
         'nose>=1.3.7',
         'nk_sent2vec>=1.1.0'
     ],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/nk-sent2vec@78d3e79ab6f59e2fac65581bb560e19fc164d640#egg=nk_sent2vec-1.1.0"
+        "git+https://github.com/NewKnowledge/nk-sent2vec@fc5acdf05bc76f5fd2dd7845f00e21413c18d4e2#egg=nk_sent2vec-1.1.0"
     ], 
     entry_points={
         'd3m.primitives': [
-            'distil.nk_sent2vec = nk_sent2vec_wrapper:nk_s2v'
+            'feature_extraction.nk_sent2vec.Nk_s2v' = nk_sent2vec_wrapper:nk_s2v'
         ],
     },
 )
