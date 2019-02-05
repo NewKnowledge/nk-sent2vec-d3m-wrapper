5575dc527dcb12582f7c6d430d0ff1567e62e36c
Numa Dhamani <numadhamani@gmail.com>
2019-02-05 11:44:03 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from distutils.core import setup
 
 setup(
-    name='nk_sent2vec_wrapper',
-    version='1.0.0',
+    name='S2Vd3mWrapper',
+    version='1.1.0',
     description='wrapper for interacting with a dockerized sent2vec text embedding primitive',
     author='New Knowledge',
-    packages=['nk_sent2vec_wrapper'],
+    packages=['S2Vd3mWrapper'],
     include_package_data=True,
     install_requires=[
         'pandas>=0.22.0, <=0.23.4',
         'numpy>=1.14.1, <=1.15.4',
         'nose>=1.3.7',
-        'nk_sent2vec>=1.1.0'
+        'd3m_sent2vec>=1.2.0'
     ],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/nk-sent2vec@fc5acdf05bc76f5fd2dd7845f00e21413c18d4e2#egg=nk_sent2vec-1.1.0"
+        "git+https://github.com/NewKnowledge/d3m_sent2vec@6b814caa4f6fe7ce460b18bcf703835f1aa14e99#egg=d3m_sent2vec-1.2.0"
     ], 
     entry_points={
         'd3m.primitives': [
-            'feature_extraction.nk_sent2vec.Nk_s2v = nk_sent2vec_wrapper:nk_s2v'
+            'feature_extraction.d3m_sent2vec.D3m_s2v = S2Vd3mWrapper:d3m_s2v'
         ],
     },
 )
