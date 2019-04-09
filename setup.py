d921f6e679f71b75b892c5e773b11a2bafb78550
numadhamani <numadhamani@gmail.com>
2019-04-09 17:20:39 +0000

    pep 508 to replace dependency_links

--- setup.py
+++ setup.py
@@ -11,10 +11,7 @@ setup(
         'pandas>=0.22.0, <=0.23.4',
         'numpy>=1.14.1, <=1.15.4',
         'nose>=1.3.7',
-        'd3m_sent2vec>=1.2.0'
-    ],
-    dependency_links=[
-        "git+https://github.com/NewKnowledge/d3m_sent2vec@414ed6aeea7c1893c05f32ea00d4d899f9d49a4f#egg=d3m_sent2vec-1.2.0"
+        'd3m_sent2vec @ git+ssh://git@github.com/NewKnowledge/d3m_sent2vec@414ed6aeea7c1893c05f32ea00d4d899f9d49a4f#egg=d3m_sent2vec-1.2.0'
     ], 
     entry_points={
         'd3m.primitives': [
